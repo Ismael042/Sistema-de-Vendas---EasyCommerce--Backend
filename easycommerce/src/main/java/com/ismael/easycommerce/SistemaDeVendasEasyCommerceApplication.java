2af74f0601663f23944a3f84d77b8054993cbb69
Ismael042 <santanaismael042@gmail.com>
2025-07-22 13:55:13 -0300

    Subindo Spring

---
+++ easycommerce/src/main/java/com/ismael/easycommerce/SistemaDeVendasEasyCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ismael.easycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVendasEasyCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVendasEasyCommerceApplication.class, args);
+	}
+
+}
---
+++ easycommerce/src/test/java/com/ismael/easycommerce/SistemaDeVendasEasyCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismael.easycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVendasEasyCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
