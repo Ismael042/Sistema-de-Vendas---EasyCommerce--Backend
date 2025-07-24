3279f6e7a79d525198e48855a07e36512bf68d6c
Ismael042 <santanaismael042@gmail.com>
2025-07-23 22:21:41 -0300

    Criando os repositorios Produto e Venda
    Interface Jpa

---
+++ easycommerce/src/main/java/com/ismael/easycommerce/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.ismael.easycommerce.repository;
+
+import com.ismael.easycommerce.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
---
+++ easycommerce/src/main/java/com/ismael/easycommerce/repository/VendaRepository.java
@@ -0,0 +1,7 @@
+package com.ismael.easycommerce.repository;
+
+import com.ismael.easycommerce.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+}
