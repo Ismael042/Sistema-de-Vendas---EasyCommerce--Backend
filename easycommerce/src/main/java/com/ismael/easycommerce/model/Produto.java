0c2d9a6edf14dd1dbd7f0c9ecd4d02912730682f
Ismael042 <santanaismael042@gmail.com>
2025-07-23 21:30:18 -0300

    Criando a Entidade Produto
    Variaveis: id, nome, preco, categoria, estoque
    Geter e Seter

---
+++ easycommerce/src/main/java/com/ismael/easycommerce/model/Produto.java
@@ -0,0 +1,54 @@
+package com.ismael.easycommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+//id, nome, preco, categoria, estoque
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String nome;
+    private BigDecimal preco;
+    private String categoria;
+    private Integer estoque;
+
+        // Geters e Seters
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public BigDecimal getPreco() {
+        return preco;
+    }
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public Integer getEstoque() {
+        return estoque;
+    }
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
