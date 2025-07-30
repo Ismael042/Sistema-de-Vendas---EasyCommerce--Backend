6ee0773da5c2590279a9d412a14828c9c7e72173
Ismael042 <santanaismael042@gmail.com>
2025-07-29 23:13:12 -0300

    Create UsuarioService.java
    Criando a rota de criar novo usuario

---
+++ backend/src/main/java/com/ismael/easycommerce/service/UsuarioService.java
@@ -0,0 +1,27 @@
+package com.ismael.easycommerce.service;
+
+import com.ismael.easycommerce.model.Usuario;
+import com.ismael.easycommerce.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario salvar(Usuario usuario){
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+
+        if (!usuario.getRole().startsWith("ROLE_")){
+            usuario.setRole("ROLE_" + usuario.getRole().toUpperCase());
+        }
+
+        return usuarioRepository.save(usuario);
+    }
+}
