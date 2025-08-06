10c5cf27037cdcd81c1fb43ed06e07f42dec9f02
Ismael042 <santanaismael042@gmail.com>
2025-08-05 23:35:12 -0300

    Update SecurityConfig.java
    Configurando a tela home

--- backend/src/main/java/com/ismael/easycommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/ismael/easycommerce/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/vendas/**","/clientes/**","/produtos/**").authenticated()
+                        .requestMatchers("/vendas/**","/clientes/**","/produtos/**","/").authenticated()
                         .anyRequest().permitAll()
                         //.requestMatchers("/usuarios/**").permitAll()
                 )
